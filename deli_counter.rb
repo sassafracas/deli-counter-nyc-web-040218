0bf45f79f6255bc5e94260168828c17f6505f703
Aleksandr Roslyakov <torpid@gmail.com>
2018-03-27 20:37:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,9 @@ def now_serving(katz_deli)
   if katz_deli.count == 0
     puts "There is nobody waiting to be served!"
   else
-     katz_deli.each do |customer|
+    katz_deli.each do |customer|
     puts "Currently serving #{customer}."
-    katz_deli.delete(0)
+    katz_deli.delete_at(0)
     end
   end
 end
